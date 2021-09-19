a50dc262067c287ce4c2d20ea0c51f10ec79db94
Tomig02 <guardotomas02@gmail.com>
2021-09-18 22:43:46 -0300

    boilerplate electron react parcel

---
+++ src/electron/dev.js
@@ -0,0 +1,31 @@
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
+
+try {
+    require('electron-reloader')(module);
+} catch (_) {}
+
+function createWindow(){
+    const win = new BrowserWindow({
+        width: 800,
+        height: 600,
+        webPreferences: {
+            nodeIntegration: true,
+            // preload: path.join(__dirname, 'preload.js')
+        }
+    });
+
+    win.loadFile('build/index.html');
+}
+
+app.on('window-all-closed', function () {
+    if (process.platform !== 'darwin') app.quit()
+});
+
+app.whenReady().then(() => {
+    createWindow()
+  
+    app.on('activate', function () {
+      if (BrowserWindow.getAllWindows().length === 0) createWindow()
+    })
+});
---
+++ src/electron/prod.js
@@ -0,0 +1,27 @@
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
+
+function createWindow(){
+    const win = new BrowserWindow({
+        width: 800,
+        height: 600,
+        webPreferences: {
+            nodeIntegration: true,
+            preload: path.join(__dirname, 'preload.js')
+        }
+    });
+
+    win.loadFile('build/index.html');
+}
+
+app.on('window-all-closed', function () {
+    if (process.platform !== 'darwin') app.quit()
+});
+
+app.whenReady().then(() => {
+    createWindow()
+  
+    app.on('activate', function () {
+      if (BrowserWindow.getAllWindows().length === 0) createWindow()
+    })
+});
