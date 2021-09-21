b2b192074c192b61c8153f3dd4430e66b9fa5edd
Tomig02 <guardotomas02@gmail.com>
2021-09-20 22:16:54 -0300

    organized scss folder, deleted icons and created new styles

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './react/App';
-import './assets/scss/styles.css';
+import './scss/styles.css';
 import {DataProvider} from './react/context/DataContext';
+
 ReactDOM.render(
     <DataProvider>
         <App />
     </DataProvider>, 
-    document.getElementById('app'));
+    document.getElementById('app')
+);
